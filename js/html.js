a54ff7a0f87d77f4e90c3bfc292b3f4b178a7ab9
yang2 <yang2000ling@163.com>
2021-02-25 11:09:19 +0800

    1

--- js/html.js
+++ js/html.js
@@ -1,38 +1,32 @@
 function navigation() {
-    document.writeln("<div><a href=\"../index.html\"><img src=\'/img/logo.png\' class=\'img-responsive\' alt=\'logo\'></a></div>");
-    document.writeln("<p class=\"lead\">&nbsp;&nbsp;有需要游戏资源和相关帮助的玩家，可在闲鱼搜索栏搜索本站官方用户：yang2000ling</p>");
-    document.writeln("		<div calss=navigation>");
-    document.writeln("			<ul class=\'nav nav-tabs\'>");
-    document.writeln("				<li><a href=\'../index.html\'>主页</a></li>");
-    document.writeln("				<li><a href=\'/pages/update.html\'>更新日志</a></li>");
-    document.writeln("			</ul>");
-    document.writeln("			<ul class=\'nav nav-tabs\'>");
-    document.writeln("				<li><a href=\'/pages/pspmain.html\'>索尼PSP游戏目录</a></li>");
-    document.writeln("				<li><a href=\'/pages/ps2main.html\'>索尼PS2游戏目录</a></li>");
-    document.writeln("				<li><a href=\'/pages/3dsch.html\'>3DS中文合集目录</a></li>");
-    document.writeln("				<li><a href=\'/pages/psvmain.html\'>索尼PSV游戏目录</a></li>");
-    document.writeln("				<li><a href=\'/pages/ps1.html\'>PS1合集目录</a></li>");
-    document.writeln("				<li><a href=\'/pages/mdcd.html\'>MDCD合集目录</a></li>");
-    document.writeln("				<li><a href=\'/pages/SSmain.html\'>世嘉SS合集目录</a></li>");
-    document.writeln("				<li><a href=\'/pages/dc.html\'>世嘉DC合集目录</a></li>");
-    document.writeln("				<li><a href=\'/pages/3do.html\'>3DO合集中英文目录</a></li>");
-    document.writeln("				<li><a href=\'/pages/neogeocd.html\'>NEOGEO CD合集中英文目录</a></li>");
-    document.writeln("				<li><a href=\'/pages/pcfx.html\'>PCFX合集中英文目录</a></li>");
-    document.writeln("				<li><a href=\'/pages/pcecd.html\'>PCE CD合集目录</a></li>");
-    document.writeln("				<li><a href=\'/pages/N64.html\'>N64合集中英文目录</a></li>");
-    document.writeln("			</ul>");
-    document.writeln("			<ul class=\'nav nav-tabs\'>");
-    document.writeln("				<li><a href=\'/jiaochen/mednafen.html\'>mednafen模拟器教程</a></li>");
-    document.writeln("				<li><a href=\'/jiaochen/retroarch.html\'>RetroArch模拟器教程</a></li>");
-    document.writeln("				<li><a href=\'/jiaochen/SSF.html\'>SSF模拟器教程</a></li>");
-    document.writeln("				<li><a href=\'/jiaochen/4DO.html\'>4DO模拟器教程</a></li>");
-    document.writeln("				<li><a href=\'/jiaochen/PSV.html\'>PSV游戏安装教程</a></li>");
-    document.writeln("			</ul>");
-    document.writeln("		</div>");
+    document.writeln("<div>");
+    document.writeln("        <!-- As a heading -->");
+    document.writeln("        <b-navbar variant=\'dark\' type=\'dark\' fixed=\'top\' toggleable=\'lg\'>");
+    document.writeln("            <b-navbar-brand>");
+    document.writeln("                <img src=\'./img/cat.jpg\' alt=\'Kitten\'>");
+    document.writeln("                <STRONG>ROM猎人</STRONG>");
+    document.writeln("            </b-navbar-brand>");
+    document.writeln("            <b-navbar-toggle target=\'nav-collapse\'></b-navbar-toggle>");
+    document.writeln("            <b-collapse id=\'nav-collapse\' is-nav>");
+    document.writeln("                <b-navbar-nav>");
+    document.writeln("                    <b-nav-item href=\'#\'>首页</b-nav-item>");
+    document.writeln("                    <b-nav-item href=\'#\'>更新日志</b-nav-item>");
+    document.writeln("                    <!-- Navbar dropdowns -->");
+    document.writeln("                    <b-nav-item-dropdown text=\'游戏目录与相关下载\' right>");
+    document.writeln("                        <b-dropdown-item href=\'#\'>掌机平台</b-dropdown-item>");
+    document.writeln("                        <b-dropdown-item href=\'#\'>家用机平台</b-dropdown-item>");
+    document.writeln("                        <b-dropdown-item href=\'#\'>街机平台</b-dropdown-item>");
+    document.writeln("                        <b-dropdown-item href=\'#\'>模拟器BIOS</b-dropdown-item>");
+    document.writeln("                    </b-nav-item-dropdown>");
+    document.writeln("                    <b-nav-item href=\'#\'>模拟器教程与下载</b-nav-item>");
+    document.writeln("                </b-navbar-nav>");
+    document.writeln("            </b-collapse>");
+    document.writeln("        </b-navbar>");
+    document.writeln("    </div>");
 }
 
 function bottom() {
     document.writeln("<div class=\'bottom text-center\'>");
-    document.writeln("			<p>如有内容侵权请致邮aeeleling@gmail.com，本站将会及时处理。<br>Powered by seele<br>Copyright © 2019-2020&nbsp&nbspyang2000ling.cn&nbsp&nbspAll Rights Reserved.</p>");
+    document.writeln("			<br><br><p>如有内容侵权请致邮aeeleling@gmail.com，本站将会及时处理。<br>Powered by seele<br>Copyright © 2019-2021&nbsp&nbspyang2000ling.com&nbsp&nbspAll Rights Reserved.</p>");
     document.writeln("		</div>");
 }
